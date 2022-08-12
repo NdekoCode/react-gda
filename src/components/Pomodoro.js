3145bf9c143705da1c52d0b515fa7e45010786bb
NdekoCode <arickbulakali@gmail.com>
2022-08-12 11:43:07 +0200

    add pomodoro structure

---
+++ src/components/Pomodoro.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Pomodoro extends Component {
+  render() {
+    return (
+      <div id="pomodoro-container ">
+        <div id="container"></div>
+      </div>
+    );
+  }
+}
