741446e4f7ce9219997578b22c2b5ebf0d472dc9
NdekoCode <arickbulakali@gmail.com>
2022-08-12 11:49:02 +0200

    add: Timer Component and adding main constructor structure and main states

---
+++ src/components/Timer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export function accurateInterval(fn, time) {
+  var cancel, nextAt, timeout, wrapper;
+  nextAt = new Date().getTime() + time;
+  timeout = null;
+  wrapper = function () {
+    nextAt += time;
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return fn();
+  };
+  cancel = function () {
+    return clearTimeout(timeout);
+  };
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+  return {
+    cancel: cancel,
+  };
+}
+
+export class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brkLength: 5,
+      seshLength: 25,
+      timerState: "stopped",
+      timerType: "Session",
+      timer: 1500,
+      intervalID: "",
+      alarmColor: { color: "white" },
+    };
+  }
+}
