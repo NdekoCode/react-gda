fc983e56f3fa678fc07e789633e6d6b5c446e741
NdekoCode <arickbulakali@gmail.com>
2022-08-12 11:45:56 +0200

    styles: pomodoro basic styles

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "../assets/css/Pomodoro.css";
 
 export class Pomodoro extends Component {
   render() {
     return (
-      <div id="pomodoro-container ">
+      <div id="pomodoro-container">
         <div id="container"></div>
       </div>
     );
