ccd1de49385998b3aa1bf7b7ba7a7feeb4ee7ad9
NdekoCode <arickbulakali@gmail.com>
2022-08-24 09:11:03 +0200

    Add outlet in services Component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// On importe l'element des liens mais aussi l'element de sortie des lient
 import { Link, Outlet } from "react-router-dom";
 
 export function Services() {
@@ -9,6 +10,7 @@ export function Services() {
         <Link to="/services/development">Services de Developpement</Link>
         <Link to="/services/marketing">Services de Marketing</Link>
       </div>
+      {/* Tous ce qui est imbriquer dans la route de "services" sera afficher dans la partie du composant Outlet */}
       <Outlet />
     </div>
   );
